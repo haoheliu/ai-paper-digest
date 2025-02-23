0f5bf0276a6b82a6c4cae49f1ed31b0dc20b0be4
haoheliu <xgdlhh@foxmail.com>
2025-02-23 18:25:03 +0000

    update

--- refresh_database.sh
+++ refresh_database.sh
@@ -1,1 +1,5 @@
+cd /home/lxb/Disk_SSD/projects/ai-paper-digest
 uv run /home/lxb/Disk_SSD/projects/llm_pdf_reader/Arxiv/processes/refresh_database.py
+git add .
+git commit -m "refresh database $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin master
