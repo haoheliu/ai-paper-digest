8b52559cee1ac5d6955b24b5b5ecc2c74520b7a3
haoheliu <xgdlhh@foxmail.com>
2025-02-24 01:23:11 +0000

    update

---
+++ commit_file.sh
@@ -0,0 +1,4 @@
+cd /home/lxb/Disk_SSD/projects/ai-paper-digest
+git add .
+git commit -m "refresh database $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin master
---
+++ serve.sh
@@ -0,0 +1,2 @@
+cd /home/lxb/Disk_SSD/projects/ai-paper-digest
+uv run /home/lxb/Disk_SSD/projects/ai-paper-dige/flask_server.py
