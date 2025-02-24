0b2dea4c9c9d628a5cb4b5498708500e4c062293
haoheliu <xgdlhh@foxmail.com>
2025-02-24 11:10:50 +0000

    refresh database 2025-02-24 11:10:50

--- flask_server.py
+++ flask_server.py
@@ -137,4 +137,4 @@ def mark_read():
     return jsonify({"message": "Paper marked as read", "list": "readed"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8082, debug=True)
